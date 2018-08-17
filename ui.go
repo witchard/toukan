9d7f315c7220d19fd945b6f88d445931322a5d62
witchard <witchard@hotmail.co.uk>
2018-08-17 19:46:38 +0100

    Add ability to move between lanes.

--- ui.go
+++ ui.go
@@ -7,43 +7,71 @@ import (
 	"github.com/rivo/tview"
 )
 
+type Lanes struct {
+	lanes  []*tview.List
+	active int
+	flex   *tview.Flex
+	app    *tview.Application
+}
+
+func NewLanes(number int, app *tview.Application) *Lanes {
+	l := &Lanes{make([]*tview.List, number), 0, tview.NewFlex(), app}
+	for i := range l.lanes {
+		l.lanes[i] = tview.NewList()
+		l.lanes[i].ShowSecondaryText(false).SetBorder(true)
+		l.lanes[i].SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+			switch event.Key() {
+			case tcell.KeyLeft:
+				l.DecActive()
+				return nil
+			case tcell.KeyRight:
+				l.IncActive()
+				return nil
+			}
+			switch event.Rune() {
+			case 'q':
+				app.Stop()
+			}
+			return event
+		})
+		for j := 0; j < 10; j++ {
+			l.lanes[i].AddItem(fmt.Sprint("Item ", j), "", 0, nil)
+		}
+		l.flex.AddItem(l.lanes[i], 0, 1, i == 0)
+	}
+
+	return l
+}
+
+func (l *Lanes) IncActive() {
+	l.active++
+	l.setActive()
+}
+
+func (l *Lanes) DecActive() {
+	l.active--
+	l.setActive()
+}
+
+func (l *Lanes) GetUi() *tview.Flex {
+	return l.flex
+}
+
+func (l *Lanes) setActive() {
+	for l.active < 0 {
+		l.active += len(l.lanes)
+	}
+	l.active %= len(l.lanes)
+	l.app.SetFocus(l.lanes[l.active])
+}
+
 func main() {
 	app := tview.NewApplication()
 
-	col1 := tview.NewList()
-	col1.ShowSecondaryText(false).SetTitle("To Do").SetBorder(true)
-	col2 := tview.NewList()
-	col2.ShowSecondaryText(false).SetTitle("Doing").SetBorder(true)
-	col3 := tview.NewList()
-	col3.ShowSecondaryText(false).SetTitle("Done").SetBorder(true)
-
-	col1.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		switch event.Key() {
-		case tcell.KeyLeft:
-			return nil
-		case tcell.KeyRight:
-			return nil
-		}
-		switch event.Rune() {
-		case 'q':
-			app.Stop()
-		}
-		return event
-	})
-
-	for i := 0; i < 10; i++ {
-		col1.AddItem(fmt.Sprint("Item ", i), "", 0, nil)
-		col2.AddItem(fmt.Sprint("Item ", i), "", 0, nil)
-		col3.AddItem(fmt.Sprint("Item ", i), "", 0, nil)
-	}
-
-	flex := tview.NewFlex().
-		AddItem(col1, 0, 1, true).
-		AddItem(col2, 0, 1, false).
-		AddItem(col3, 0, 1, false)
+	lanes := NewLanes(3, app)
 
 	pages := tview.NewPages().
-		AddPage("TouKan", flex, true, true)
+		AddPage("TouKan", lanes.GetUi(), true, true)
 	app.SetRoot(pages, true)
 
 	if err := app.Run(); err != nil {
